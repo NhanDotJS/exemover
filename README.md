c6acfdc3974407ce12732607bdd5cf86b57dc7d1
Thanh Nhan Le <thanhnhanle1412@gmail.com>
2021-08-17 19:25:01 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Auto Exe Mover
+Require: Python 3.6+
+
+```
+pip install watchdog
+python main.py
+
+```
---
+++ main.py
@@ -0,0 +1,31 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+import time
+import shutil
+
+if os.path.isdir('exefile'):
+    try:
+        os.mkdir("./exefile")
+    except OSError as error:
+        print(error)
+
+
+class MoveFile(FileSystemEventHandler):
+    def on_created(self, event):
+        for filename in os.listdir("."):
+            f_name, f_ext = os.path.splitext(filename)
+            if f_ext == ".exe":
+                shutil.move(filename, "./exefile/" + filename)
+
+
+observer = Observer()
+observer.schedule(MoveFile(), ".", recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join
